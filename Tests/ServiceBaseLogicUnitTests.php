4d56af8752c79415daa38d0bedebee3d008fc9cd
gdever <gdever@DESKTOP-GAII657>
2020-04-11 15:43:42 +0300

    bugfix/Tests were into PSR-4 compatible parts

--- Tests/ServiceBaseLogicUnitTests.php
+++ Tests/ServiceBaseLogicUnitTests.php
@@ -12,48 +12,6 @@ namespace Mezon\Service\Tests;
  */
 
 /**
- * Mock parameter fetcher
- *
- * @author Dodonov A.A.
- * @group baseTests
- */
-class MockParamsFetcher implements \Mezon\Service\ServiceRequestParamsInterface
-{
-
-    /**
-     * Some testing value
-     *
-     * @var string
-     */
-    protected $value = false;
-
-    /**
-     * Constructor
-     *
-     * @param string $value
-     *            Value to be set
-     */
-    public function __construct($value = 'value')
-    {
-        $this->value = $value;
-    }
-
-    /**
-     * Method returns request parameter
-     *
-     * @param string $param
-     *            parameter name
-     * @param mixed $default
-     *            default value
-     * @return mixed Parameter value
-     */
-    public function getParam($param, $default = false)
-    {
-        return $this->value;
-    }
-}
-
-/**
  * Mock model
  *
  * @author Dodonov A.A.
