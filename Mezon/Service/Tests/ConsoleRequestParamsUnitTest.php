3058c7a0ec7c7a90fdc1f98782d7794ae9c8dd5a
gdever <alexey@dodonov.pro>
2022-01-14 18:17:50 +0300

    bugfix/Unit-test was fixed

--- Mezon/Service/Tests/ConsoleRequestParamsUnitTest.php
+++ Mezon/Service/Tests/ConsoleRequestParamsUnitTest.php
@@ -19,6 +19,8 @@ class ConsoleRequestParamsUnitTest extends \PHPUnit\Framework\TestCase
         // setup
         $router = new Router();
         $requestParams = new ConsoleRequestParams($router);
+        global $argv;
+        $argv[0] = './vendor/phpunit/phpunit/phpunit';
 
         // test body
         $result = $requestParams->getParam(0);
